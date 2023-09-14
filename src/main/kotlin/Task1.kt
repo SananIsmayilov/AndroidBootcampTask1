2a808ecf4eca219343ac8e86e208e7633b52a9be
Sanan <116113098+SananIsmayilov@users.noreply.github.com>
2023-09-14 21:40:43 +0400

    Task complete

---
+++ src/main/kotlin/Task1.kt
@@ -0,0 +1,49 @@
+fun main() {
+    val rayon : String = "Saatlı"
+    println("Rayon : $rayon ")
+
+    val qite : String = "Asiya"
+    println("Qitə : $qite ")
+
+
+    val poct_Kodu : Int = 49
+    println("Poçt kodu  : $poct_Kodu ")
+
+    val insta_unvan : String = "_i.sanan_"
+    println("İnstagram hesabı : $insta_unvan ")
+
+
+    val dogum_tarixi : String = "07.10.2004"
+    println("Doğum tarixi : $dogum_tarixi ")
+
+
+    val telefon_modeli : String = "Samsung"
+    println("Telefon modeli : $telefon_modeli ")
+
+
+    val yemek : String = "XXXXX :)"
+    println("Yemək adı : $yemek ")
+
+
+
+    val reng : String = "Yaşıl"
+    println("Rəng : $reng ")
+
+
+
+    val komp_model : String = "Dell"
+    println("Kompüter modeli : $komp_model ")
+
+
+    val ekranolcusu : Double = 15.6
+    println("Ekran ölçüsü : $ekranolcusu ")
+
+
+    val istifademuddeti : Int = 12
+    println("Istifadə müddəti  : $istifademuddeti  ay ")
+
+
+
+
+
+}
