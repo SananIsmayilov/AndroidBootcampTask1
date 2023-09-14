ddd456d50996451a1f0f8949c8338a3a5bbad797
Sanan <116113098+SananIsmayilov@users.noreply.github.com>
2023-09-14 21:43:46 +0400

    Code reformatted

--- src/main/kotlin/Task1.kt
+++ src/main/kotlin/Task1.kt
@@ -1,49 +1,44 @@
 fun main() {
-    val rayon : String = "Saatlı"
+    val rayon: String = "Saatlı"
     println("Rayon : $rayon ")
 
-    val qite : String = "Asiya"
+    val qite: String = "Asiya"
     println("Qitə : $qite ")
 
 
-    val poct_Kodu : Int = 49
+    val poct_Kodu: Int = 49
     println("Poçt kodu  : $poct_Kodu ")
 
-    val insta_unvan : String = "_i.sanan_"
+    val insta_unvan: String = "_i.sanan_"
     println("İnstagram hesabı : $insta_unvan ")
 
 
-    val dogum_tarixi : String = "07.10.2004"
+    val dogum_tarixi: String = "07.10.2004"
     println("Doğum tarixi : $dogum_tarixi ")
 
 
-    val telefon_modeli : String = "Samsung"
+    val telefon_modeli: String = "Samsung"
     println("Telefon modeli : $telefon_modeli ")
 
 
-    val yemek : String = "XXXXX :)"
+    val yemek: String = "XXXXX :)"
     println("Yemək adı : $yemek ")
 
 
-
-    val reng : String = "Yaşıl"
+    val reng: String = "Yaşıl"
     println("Rəng : $reng ")
 
 
-
-    val komp_model : String = "Dell"
+    val komp_model: String = "Dell"
     println("Kompüter modeli : $komp_model ")
 
 
-    val ekranolcusu : Double = 15.6
+    val ekranolcusu: Double = 15.6
     println("Ekran ölçüsü : $ekranolcusu ")
 
 
-    val istifademuddeti : Int = 12
+    val istifademuddeti: Int = 12
     println("Istifadə müddəti  : $istifademuddeti  ay ")
-
-
-
 
 
 }
